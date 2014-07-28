084870bd346d5906e9486228e0019e93bc53ab14
Jeroen Dekkers <jeroen@dekkers.ch>
2014-07-28 20:07:34 +0200

    Add production settings

--- ticketshop/settings/prod.py
+++ ticketshop/settings/prod.py
@@ -11,8 +11,8 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'NAME': 'ticketshop',                      # Or path to database file if using sqlite3.
+        'USER': 'ticketshop',                      # Not used with sqlite3.
         'PASSWORD': DATABASE_PASSWORD,                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -20,7 +20,4 @@ DATABASES = {
     }
 }
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT =
-STATIC_ROOT =
+STATIC_ROOT = '/srv/static/ticketshop'
