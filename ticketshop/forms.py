bcb4bdf0b1d26050187c674c8c40766d0c1ff380
Jeroen Dekkers <jeroen@dekkers.ch>
2014-07-28 21:16:42 +0200

    Fix problem with empty donation field

--- ticketshop/forms.py
+++ ticketshop/forms.py
@@ -5,7 +5,7 @@ from django import forms
 
 class OrderForm(forms.Form):
     bar_credits = forms.TypedChoiceField(choices=((i, i) for i in range(6)), coerce=int, empty_value=0)
-    donation = forms.IntegerField(initial=0, min_value=0)
+    donation = forms.IntegerField(initial=0, min_value=0, required=False)
     conditions = forms.BooleanField()
 
     def __init__(self, *args, **kwargs):
@@ -15,6 +15,14 @@ class OrderForm(forms.Form):
             field = forms.TypedChoiceField(choices=((i, i) for i in range(6)), coerce=int, empty_value=0)
             field.ticket = ticket
             self.fields['ticket_%d' % ticket.id] = field
+
+    def clean_donation(self):
+        # Not filling in anything should be treated the same as zero.
+        donation = self.cleaned_data['donation']
+        if not donation:
+            return 0
+        else:
+            return donation
 
     def ticket_fields(self):
         for name in self.fields:
