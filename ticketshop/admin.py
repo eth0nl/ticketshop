9536d9d33f4afb22dddfe56d430926b093d0dd09
Jeroen Dekkers <jeroen@dekkers.ch>
2019-10-18 20:18:33 +0200

    Also show table of used tickets in stats

--- ticketshop/admin.py
+++ ticketshop/admin.py
@@ -37,33 +37,48 @@ def stats(request):
     context['tickets'] = []
     context['total'] = 0
     context['total_paid'] = 0
+    context['total_used'] = 0
     context['subtotal_count'] = 0
     context['subtotal_count_paid'] = 0
+    context['subtotal_count_used'] = 0
     for ticket in TicketType.objects.filter(event=event):
         count = Ticket.objects.filter(order__event=event, order__status__in=(Order.PAID, Order.USED, Order.PENDING), type=ticket).aggregate(count=Sum('count'))['count']
         count_paid = Ticket.objects.filter(order__event=event, order__status__in=(Order.PAID, Order.USED), type=ticket).aggregate(count=Sum('count'))['count'] or 0
+        count_used = Ticket.objects.filter(order__event=event, order__status__in=(Order.USED, ), type=ticket).aggregate(count=Sum('count'))['count'] or 0
         if count:
-            context['tickets'].append({'count': count, 'total': count * ticket.price, 'name': ticket.name, 'price': ticket.price,
-                                       'count_paid': count_paid, 'total_paid': count_paid * ticket.price})
+            context['tickets'].append({
+                'count': count, 'total': count * ticket.price, 'name': ticket.name, 'price': ticket.price,
+                'count_paid': count_paid, 'total_paid': count_paid * ticket.price,
+                'count_used': count_used, 'total_used': count_used * ticket.price
+            })
             context['total'] += count * ticket.price
             context['subtotal_count'] += count
             context['total_paid'] += count_paid * ticket.price
             context['subtotal_count_paid'] += count_paid
+            context['total_used'] += count_used * ticket.price
+            context['subtotal_count_used'] += count_used
 
     context['subtotal_tickets'] = context['total']
     context['subtotal_tickets_paid'] = context['total_paid']
+    context['subtotal_tickets_used'] = context['total_used']
     context['bar_credits'] = Order.objects.filter(event=event, status__in=(Order.PAID, Order.USED, Order.PENDING)).aggregate(bar_credits=Sum('bar_credits'))['bar_credits']
     context['bar_credits_paid'] = Order.objects.filter(event=event, status__in=(Order.PAID, Order.USED)).aggregate(bar_credits=Sum('bar_credits'))['bar_credits']
+    context['bar_credits_used'] = Order.objects.filter(event=event, status__in=(Order.USED, )).aggregate(bar_credits=Sum('bar_credits'))['bar_credits']
     context['donation'] = Order.objects.filter(event=event, status__in=(Order.PAID, Order.USED, Order.PENDING)).aggregate(donation=Sum('donation'))['donation']
     context['donation_paid'] = Order.objects.filter(event=event, status__in=(Order.PAID, Order.USED)).aggregate(donation=Sum('donation'))['donation']
+    context['donation_used'] = Order.objects.filter(event=event, status__in=(Order.USED, )).aggregate(donation=Sum('donation'))['donation']
     if context['bar_credits']:
         context['total'] += context['bar_credits']
     if context['bar_credits_paid']:
         context['total_paid'] += context['bar_credits_paid']
+    if context['bar_credits_used']:
+        context['total_used'] += context['bar_credits_used']
     if context['donation']:
         context['total'] += context['donation']
     if context['donation_paid']:
         context['total_paid'] += context['donation_paid']
+    if context['donation_used']:
+        context['total_used'] += context['donation_used']
 
     return render_to_response('ticketshop/admin/stats.html',
                               context,
